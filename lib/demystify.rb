1548fdc4b9924c6dfe7a56475626c2d3a3f4d914
Douglas Gordon <douglastgordon@gmail.com>
2016-12-02 17:21:33 -0500

    get chars

--- lib/demystify.rb
+++ lib/demystify.rb
@@ -2,14 +2,19 @@ module Demystify
 
   class Text
 
-    attr_accessor :content
+    attr_accessor :content, :chars
 
     def initialize(file)
-      @content = File.open(file)
+      @content = open(file).read
+      @chars = @content.split("")
     end
+
+  
 
   end
 
 end
 
 something = Demystify::Text.new(File.join( File.dirname(__FILE__), '../sample1.txt'))
+
+p something.chars
